cb6cd54dc181dc4d0005a4c6aa2724899cd75be5
ZakiexK <90477282+ZakiexK@users.noreply.github.com>
2023-07-31 00:52:37 +0530

    Add files via upload

---
+++ springboot-backend/src/main/java/net/springboot/entity/Student.java
@@ -0,0 +1,51 @@
+package net.springboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    private int rollNo;
+    private String name;
+    private String address;
+
+    public int getRollNo() {
+        return rollNo;
+    }
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public Student(int rollNo, String name, String address) {
+        super();
+        this.rollNo = rollNo;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Student ()
+    {
+        
+    }
+    @Override
+    public String toString() {
+        return "Student [rollNo=" + rollNo + ", name=" + name + ", address=" + address + "]";
+    }
+
+    
+
+       
+}
